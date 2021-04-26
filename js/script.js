6df43cdc46a45ea0425a5992e2139f12ce5277fb
fmarabini93 <fmarabini93>
2021-04-26 14:34:42 +0200

    passwd-gen

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name?");
+var favColor = prompt("Which's your favourite color?");
+document.getElementById("first-name").innerHTML = firstName;
+document.getElementById("last-name").innerHTML = lastName;
+document.getElementById("fav-color").innerHTML = favColor;
