caebcb6a3ec24010e6488331ac6c754014de1597
fmarabini93 <fmarabini93>
2021-04-26 18:42:49 +0200

    css-decoration

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+alert("This is only a temporary and unsecure password, please change it ASAP")
 var firstName = prompt("What's your first name?");
 var lastName = prompt("What's your last name?");
 var favColor = prompt("Which's your favourite color?");
