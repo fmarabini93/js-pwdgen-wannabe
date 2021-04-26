bd687fad9bfb4e8e1ee026b227512dddfd878b35
fmarabini93 <fmarabini93>
2021-04-26 14:38:47 +0200

    touch-up

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 var firstName = prompt("What's your first name?");
 var lastName = prompt("What's your last name?");
 var favColor = prompt("Which's your favourite color?");
-document.getElementById("first-name").innerHTML = firstName;
-document.getElementById("last-name").innerHTML = lastName;
-document.getElementById("fav-color").innerHTML = favColor;
+document.getElementById("passwd").innerHTML = firstName + lastName + favColor;
